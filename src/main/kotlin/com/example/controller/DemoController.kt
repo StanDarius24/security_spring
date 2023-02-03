b9b31c7cbdcf2728582fd21b825108b0ee139073
StanDarius24 <standarius24@gmail.com>
2023-02-03 19:06:11 +0200

    more about method authorization

--- src/main/kotlin/com/example/controller/DemoController.kt
+++ src/main/kotlin/com/example/controller/DemoController.kt
@@ -1,5 +1,6 @@
 package com.example.controller
 
+import org.springframework.security.access.prepost.PostAuthorize
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,13 +12,13 @@ class DemoController {
     @GetMapping("/hello")
     @PreAuthorize("hasAuthority('read')")
     fun hello(): String {
-        return "Hello World!"
+        return "Hello World 22!"
     }
 
     @GetMapping("/demo")
     @PreAuthorize("hasAnyAuthority('read', 'write')")
     fun demo(): String {
-        return "Hello Demo!"
+        return "Hello Demo 21!"
     }
 
     @GetMapping("/demo2/{something}")
@@ -28,15 +29,21 @@ class DemoController {
             """
     ) // not recommended hard to debug
     fun index(@PathVariable("something") something: String): String {
-        return "Hello Index!"
+        return "Hello Index 1!"
     }
 
     // do this instead
     @GetMapping("/demo3/{something}")
     @PreAuthorize("@demo4ConditionEvaluator.condition()") // using a bean
     fun index2(@PathVariable("something") something: String): String {
-        return "Hello Index2!"
+        return "Hello Index 2!"
     }
 
+    @GetMapping("/demo4")
+    @PostAuthorize("returnObject == 'Hello Index 3!'") // is mainly used when we want to restrict the access to some returned value
+    fun index3(): String {
+        println(":)") // never use @PostAuthorize with methods that change data in the database
+        return "Hello Index 3!"
+    }
 
 }
