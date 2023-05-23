051f91ddab1f8d148d4676d0e960f60bc49fa8db
Stannis24 <standarius24@gmail.com>
2023-05-23 16:39:44 +0300

    2 authorization servers, one with opaque, the other non-opaque

--- resourceServer/src/main/kotlin/com/stannis/resourceserver/config/ProjectConfig.kt
+++ resourceServer/src/main/kotlin/com/stannis/resourceserver/config/ProjectConfig.kt
@@ -3,35 +3,72 @@ package com.stannis.resourceserver.config
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationManagerResolver
+import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider
+import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector
+import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector
 import org.springframework.security.web.SecurityFilterChain
+
 
 @Configuration
 class ProjectConfig {
-
     @Bean
+    @Throws(Exception::class)
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.oauth2ResourceServer{
-            it.authenticationManagerResolver(authenticationManagerResolver()) // can manage multiple application providers
+        http.oauth2ResourceServer { j: OAuth2ResourceServerConfigurer<HttpSecurity?> ->
+            j.authenticationManagerResolver(
+                authenticationManagerResolver(jwtDecoder(), opaqueTokenIntrospector())
+            )
         }
-
+        http.authorizeHttpRequests().anyRequest().authenticated()
         return http.build()
     }
 
+    //  @Bean
+    //  public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver() {
+    //    JwtIssuerAuthenticationManagerResolver a =
+    //        new JwtIssuerAuthenticationManagerResolver("http://localhost:8081", "http://localhost:8082");
+    //
+    //    return a;
+    //  }
     @Bean
-    fun authenticationManagerResolver(): AuthenticationManagerResolver<HttpServletRequest> {
-        val a = JwtIssuerAuthenticationManagerResolver("http://localhost:8080", "http://localhost:8081")
-
-        return a
+    fun authenticationManagerResolver(
+        jwtDecoder: JwtDecoder?, opaqueTokenIntrospector: OpaqueTokenIntrospector?
+    ): AuthenticationManagerResolver<HttpServletRequest> {
+        val jwtAuth: AuthenticationManager = ProviderManager(
+            JwtAuthenticationProvider(jwtDecoder)
+        )
+        val opaqueAuth: AuthenticationManager = ProviderManager(
+            OpaqueTokenAuthenticationProvider(opaqueTokenIntrospector)
+        )
+        return AuthenticationManagerResolver { request: HttpServletRequest ->
+            if ("jwt" == request.getHeader("type")) {
+                return@AuthenticationManagerResolver jwtAuth
+            } else {
+                return@AuthenticationManagerResolver opaqueAuth
+            }
+        }
     }
 
-//    @Bean
-//    fun authManagerResolver(): AuthenticationManagerResolver<HttpServletRequest> {
-//        val a = ProviderManager(JwtAuthenticationProvider(JwtDecoder))
-//
-//        return a -> r
-//    }
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        return NimbusJwtDecoder
+            .withJwkSetUri("http://localhost:8081/oauth2/jwks")
+            .build()
+    }
 
+    @Bean
+    fun opaqueTokenIntrospector(): OpaqueTokenIntrospector {
+        return SpringOpaqueTokenIntrospector(
+            "http://localhost:8082/oauth2/introspect",
+            "client", "secret"
+        )
+    }
 }
